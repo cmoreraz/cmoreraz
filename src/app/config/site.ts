5891ab7c41bb9c755ef66263264ed38ced4b77b0
Software Engineer and Test Automation Engineer <61637059+cmoreraz@users.noreply.github.com>
2024-01-17 14:19:06 +0000

    create

--- src/app/config/site.ts
+++ src/app/config/site.ts
@@ -3,8 +3,8 @@ import { SiteConfig } from "../../../old/types";
 
 
 export const siteConfig: SiteConfig = {
-  name: "Richard B. Vinueza | Portfolio NextJS",
-  title: "Richard B. Vinueza",
+  name: "Cristian Andres Morera | Portfolio NextJS",
+  title: "Cristian Andres Morera",
   description:
     "Open Source Portfolio - Creative developer since 2014 and Freelance | Software Engineer with several years of experience as a Freelancer. Multiple certifications. Passionate about traveling and naturally curious. Currently, I am focused on becoming a great leader and continuing to share.",
   url: "https://richard.darkpixl.com",
