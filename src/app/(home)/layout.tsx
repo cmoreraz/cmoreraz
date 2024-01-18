a1b30feb94993407f0a36364243774310d13bd4a
Software Engineer and Test Automation Engineer <61637059+cmoreraz@users.noreply.github.com>
2024-01-18 17:36:10 +0000

    init

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/ui/footer/footer"
 import { NavBar } from "@/components/ui/navbar/navbar"
 
 export default function RootLayout({
@@ -7,11 +8,13 @@ export default function RootLayout({
 }) {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-3">
-            <div className="relative z-20 flex w-full flex-col  items-center justify-center gap-4">
 
+            <div className="relative z-20 flex w-full flex-col  items-center justify-center gap-4">
                 <NavBar />
             </div>
+
             {children}
+
         </main>
     )
 }
