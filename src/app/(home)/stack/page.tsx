77060ea4dcea8f51e0cfa98e168db02cc151c44a
Software Engineer and Test Automation Engineer <61637059+cmoreraz@users.noreply.github.com>
2024-01-19 17:14:54 +0000

    init

--- src/app/(home)/stack/page.tsx
+++ src/app/(home)/stack/page.tsx
@@ -1,33 +1,66 @@
+import Image from "next/image";
+
 const Stack = () => {
     return (
         <section
             id="stack"
             className="container space-y-6 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24"
         >
-            <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
-                <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
-                    Frameworks & Tech
+            <div className="mx-auto flex max-w-[58rem] flex-col space-y-4">
+                <h2 className="text-3xl font-bold text-onyx dark:text-white md:text-5xl">
+                    Dev & Design
                 </h2>
-                <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
-                    These are the tools and frameworks with which I feel more comfortable
-                    without limiting myself to them because I have worked with Angular,
-                    MySql, Laravel, C++ and many more... My main stack is:
-                </p>
             </div>
+
             <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
-                <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-                    <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-                        <svg viewBox="0 0 24 24" className="h-12 w-12 fill-current">
-                            <path d="M11.572 0c-.176 0-.31.001-.358.007a19.76 19.76 0 0 1-.364.033C7.443.346 4.25 2.185 2.228 5.012a11.875 11.875 0 0 0-2.119 5.243c-.096.659-.108.854-.108 1.747s.012 1.089.108 1.748c.652 4.506 3.86 8.292 8.209 9.695.779.25 1.6.422 2.534.525.363.04 1.935.04 2.299 0 1.611-.178 2.977-.577 4.323-1.264.207-.106.247-.134.219-.158-.02-.013-.9-1.193-1.955-2.62l-1.919-2.592-2.404-3.558a338.739 338.739 0 0 0-2.422-3.556c-.009-.002-.018 1.579-.023 3.51-.007 3.38-.01 3.515-.052 3.595a.426.426 0 0 1-.206.214c-.075.037-.14.044-.495.044H7.81l-.108-.068a.438.438 0 0 1-.157-.171l-.05-.106.006-4.703.007-4.705.072-.092a.645.645 0 0 1 .174-.143c.096-.047.134-.051.54-.051.478 0 .558.018.682.154.035.038 1.337 1.999 2.895 4.361a10760.433 10760.433 0 0 0 4.735 7.17l1.9 2.879.096-.063a12.317 12.317 0 0 0 2.466-2.163 11.944 11.944 0 0 0 2.824-6.134c.096-.66.108-.854.108-1.748 0-.893-.012-1.088-.108-1.747-.652-4.506-3.859-8.292-8.208-9.695a12.597 12.597 0 0 0-2.499-.523A33.119 33.119 0 0 0 11.573 0zm4.069 7.217c.347 0 .408.005.486.047a.473.473 0 0 1 .237.277c.018.06.023 1.365.018 4.304l-.006 4.218-.744-1.14-.746-1.14v-3.066c0-1.982.01-3.097.023-3.15a.478.478 0 0 1 .233-.296c.096-.05.13-.054.5-.054z" />
+
+                <div className="flex h-[300px] flex-col justify-between rounded-md p-6">
+                    <a aria-label="Figma" target="_blank" className="group relative flex items-center justify-center gap-4 overflow-hidden rounded-3xl border-[1px] dark:border-card-border border-light-card-border bg-faint-white dark:bg-onyx p-4 transition duration-300 dark:hover:bg-white/10 hover:bg-onyx/5 md:h-[300px] md:p-6" href="https://figma.com/">
+
+                        <picture className="relative block h-[180px] w-[60px] origin-bottom transition duration-300 md:h-[100px] md:w-[100px] md:group-hover:-translate-y-1 md:group-hover:scale-[101%]">
+                            <Image width={100} height={100} alt="Figma" src="./images/figma.webp" />
+                        </picture>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="absolute right-6 top-6 hidden -translate-x-2 translate-y-2 rotate-45 text-dark-gray opacity-0 transition duration-300 hover:rotate-[405deg] group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100 dark:text-white md:inline-block">
+                            <path d="m5 12 7-7 7 7" />
+                            <path d="M12 19V5" />
                         </svg>
-                        <div className="space-y-2">
-                            <h3 className="font-bold">Next.js</h3>
-                            <p className="text-sm text-muted-foreground">
-                                App dir, Routing, Layouts, Loading UI and API routes.
+
+                        <div className="relative flex flex-1 items-center justify-between md:absolute md:inset-x-6 md:bottom-6">
+                            <p className="text-base font-semibold text-onyx dark:text-white md:text-xl">
+                                Figma
                             </p>
+                            <span className="inline-block rounded-full border-[1px] border-light-card-border bg-white/50 px-3 py-[6px] text-xs text-dark-gray dark:border-[#272525] dark:bg-transparent md:text-sm">
+                                Design
+                            </span>
                         </div>
-                    </div>
+                    </a>
                 </div>
+
+                <div className="flex h-[300px] flex-col justify-between rounded-md p-6">
+                    <a aria-label="Figma" target="_blank" className="group relative flex items-center justify-center gap-4 overflow-hidden rounded-3xl border-[1px] dark:border-card-border border-light-card-border bg-faint-white dark:bg-onyx p-4 transition duration-300 dark:hover:bg-white/10 hover:bg-onyx/5 md:h-[300px] md:p-6" href="https://figma.com/">
+
+                        <picture className="relative block h-[180px] w-[60px] origin-bottom transition duration-300 md:h-[100px] md:w-[100px] md:group-hover:-translate-y-1 md:group-hover:scale-[101%]">
+                            <Image width={100} height={100} alt="NextJs" src="./images/nextjs.webp" />
+                        </picture>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="absolute right-6 top-6 hidden -translate-x-2 translate-y-2 rotate-45 text-dark-gray opacity-0 transition duration-300 hover:rotate-[405deg] group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100 dark:text-white md:inline-block">
+                            <path d="m5 12 7-7 7 7" />
+                            <path d="M12 19V5" />
+                        </svg>
+
+                        <div className="relative flex flex-1 items-center justify-between md:absolute md:inset-x-6 md:bottom-6">
+                            <p className="text-base font-semibold text-onyx dark:text-white md:text-xl">
+                            Next.js
+                            </p>
+                            <span className="inline-block rounded-full border-[1px] border-light-card-border bg-white/50 px-3 py-[6px] text-xs text-dark-gray dark:border-[#272525] dark:bg-transparent md:text-sm">
+                            Web Framework
+                            </span>
+                        </div>
+                    </a>
+                </div>
+
+
                 <div className="relative overflow-hidden rounded-lg border bg-background p-2">
                     <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
                         {
