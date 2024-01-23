3d27751a4fe7068266c6d2b83a1aa946083c74ea
Software Engineer and Test Automation Engineer <61637059+cmoreraz@users.noreply.github.com>
2024-01-23 00:00:20 +0000

    init

--- src/app/(home)/experience/page.tsx
+++ src/app/(home)/experience/page.tsx
@@ -50,14 +50,14 @@ export default function Experience() {
               <div className="ml-6">
                 <h4 className="font-bold">Software Development Engineer in Test.</h4>
                 <p className="mt-2 max-w-screen-sm text-sm text-gray-500">Dirigí el equipo de automatización compuesto por 5 colaboradores, encargándome de definir e implementar la estrategia de pruebas.
-                  • Llevé a cabo la implementación exitosa del sistema de CI/CD mediante la herramienta Jenkins, lo que permitió la ejecución de pruebas automáticas en entornos de QA y Producción. Esta iniciativa aumentó la efectividad del equipo de calidad en un destacado 40%.
-                  • Diseñé y desarrollé el framework de automatización "Giru Giru", el cual incrementó la efectividad del equipo de automatización en un 30% y redujo la duplicidad de código en un impresionante 80%.
-                  • Desarrollé una aplicación REST y un Scraper para facilitar la compra de CTL's a través de la página de la SNR. Este proyecto redujo la recompra de 12,000 a 5,000 CTL's, mejorando la efectividad de los procesos internos en un significativo 80%.
-                  • Creé un scraper que permitió obtener el valor de los recibos públicos de los inmuebles adquiridos por la compañía, automatizando el 90% del proceso y liberando a 4 colaboradores para otras actividades.
-                  • Automatizé el 100% del proceso de firma de documentos, liberando así a dos colaboradores para otras tareas.
-                  • Implementé la herramienta GLPI para el manejo de tickets en el área legal, logrando aumentar la efectividad de un equipo de 18 personas en un 20%.
-                  • Proporcioné mentoría y orientación a otros miembros del equipo, compartiendo conocimientos y buenas prácticas para fomentar el crecimiento y desarrollo profesional.
-                  • Implementé prácticas de control de versiones utilizando sistemas como Git.</p>
+                   Llevé a cabo la implementación exitosa del sistema de CI/CD mediante la herramienta Jenkins, lo que permitió la ejecución de pruebas automáticas en entornos de QA y Producción. Esta iniciativa aumentó la efectividad del equipo de calidad en un destacado 40%.
+                   Diseñé y desarrollé el framework de automatización &apos;Giru Giru, el cual incrementó la efectividad del equipo de automatización en un 30% y redujo la duplicidad de código en un impresionante 80%.
+                   Desarrollé una aplicación REST y un Scraper para facilitar la compra de CTL&apos;s a través de la página de la SNR. Este proyecto redujo la recompra de 12,000 a 5,000 CTL&apos;s, mejorando la efectividad de los procesos internos en un significativo 80%.
+                   Creé un scraper que permitió obtener el valor de los recibos públicos de los inmuebles adquiridos por la compañía, automatizando el 90% del proceso y liberando a 4 colaboradores para otras actividades.
+                   Automatizé el 100% del proceso de firma de documentos, liberando así a dos colaboradores para otras tareas.
+                   Implementé la herramienta GLPI para el manejo de tickets en el área legal, logrando aumentar la efectividad de un equipo de 18 personas en un 20%.
+                   Proporcioné mentoría y orientación a otros miembros del equipo, compartiendo conocimientos y buenas prácticas para fomentar el crecimiento y desarrollo profesional.
+                   Implementé prácticas de control de versiones utilizando sistemas como Git.</p>
                 <span className="mt-1 block text-sm font-semibold text-blue-500">2021 - 2023</span>
               </div>
             </div>
@@ -70,11 +70,11 @@ export default function Experience() {
                   Test Automation Engineer.
                 </h4>
                 <p className="mt-2 max-w-screen-sm text-sm text-gray-500"> Lideré un equipo de automatización compuesto por 3 colaboradores, encargándome de definir e implementar la estrategia de pruebas.
-                  • Realicé la implementación exitosa del sistema de CI/CD mediante la herramienta Jenkins. Esto posibilitó la ejecución de pruebas automáticas en entornos de QA y Producción, incrementando la efectividad del equipo en un notable 30%.
-                  • Implementé y gestioné las herramientas Bugzilla y Testlink para el seguimiento de planes de prueba y bugs. Este cambio estratégico eliminó la necesidad de manejar archivos de Excel, proporcionando visibilidad y transparencia a todas las áreas involucradas y automatizando el proceso en su totalidad.
-                  • Establecí KPIs de calidad para medir el número de bugs y análisis estático, logrando mitigar la deuda técnica en un significativo 20%.
-                  • Implementé y mantuve herramientas de automatización de pruebas, como Selenium, JUnit, Git, Rest Assured y Appium, reduciendo el esfuerzo del equipo de calidad en un 30%.
-                  • Proporcioné capacitación y orientación técnica al equipo de QA, compartiendo conocimientos y fomentando el crecimiento y desarrollo profesional. Transformé los roles de QA en roles integrales con la capacidad de realizar mantenimiento a las automatizaciones.</p>
+                   Realicé la implementación exitosa del sistema de CI/CD mediante la herramienta Jenkins. Esto posibilitó la ejecución de pruebas automáticas en entornos de QA y Producción, incrementando la efectividad del equipo en un notable 30%.
+                   Implementé y gestioné las herramientas Bugzilla y Testlink para el seguimiento de planes de prueba y bugs. Este cambio estratégico eliminó la necesidad de manejar archivos de Excel, proporcionando visibilidad y transparencia a todas las áreas involucradas y automatizando el proceso en su totalidad.
+                   Establecí KPIs de calidad para medir el número de bugs y análisis estático, logrando mitigar la deuda técnica en un significativo 20%.
+                   Implementé y mantuve herramientas de automatización de pruebas, como Selenium, JUnit, Git, Rest Assured y Appium, reduciendo el esfuerzo del equipo de calidad en un 30%.
+                   Proporcioné capacitación y orientación técnica al equipo de QA, compartiendo conocimientos y fomentando el crecimiento y desarrollo profesional. Transformé los roles de QA en roles integrales con la capacidad de realizar mantenimiento a las automatizaciones.</p>
                 <span className="mt-1 block text-sm font-semibold text-blue-500">2021- 2021</span>
               </div>
             </div>
@@ -85,11 +85,11 @@ export default function Experience() {
               <div className="ml-6">
                 <h4 className="font-bold">Junior Software Testing Analyst.</h4>
                 <p className="mt-2 max-w-screen-sm text-sm text-gray-500">Responsable de garantizar la calidad del producto mediante la creacion de planes y casos de prueba.
-                  • Coordiné actividades de pruebas e implementé scripts automatizados de prueba utilizando herramientas como Serenity BDD, Postman y Jmeter, especialmente en el procesamiento de pagos con tarjetas de crédito, débito, cuentas de ahorro y cuentas corrientes.
-                  • Automatización del reporte de tiempos semanales del equipo de calidad, liberando 30 horas para otras actividades.
-                  • Creación de un script de pruebas automatizadas para validar la estabilidad de los ambientes de prueba, reduciendo 30 horas de pruebas manuales.
-                  • Implementé un repositorio que contiene consultas a bases de datos en la herramienta SQL Server, aumentando la productividad del equipo en un 5%.
-                  • Diseño e implementación de una base de conocimiento y capacitación en SharePoint para nuevos integrantes del equipo de calidad, optimizando la curva de aprendizaje en un 60%.</p>
+                   Coordiné actividades de pruebas e implementé scripts automatizados de prueba utilizando herramientas como Serenity BDD, Postman y Jmeter, especialmente en el procesamiento de pagos con tarjetas de crédito, débito, cuentas de ahorro y cuentas corrientes.
+                   Automatización del reporte de tiempos semanales del equipo de calidad, liberando 30 horas para otras actividades.
+                   Creación de un script de pruebas automatizadas para validar la estabilidad de los ambientes de prueba, reduciendo 30 horas de pruebas manuales.
+                   Implementé un repositorio que contiene consultas a bases de datos en la herramienta SQL Server, aumentando la productividad del equipo en un 5%.
+                   Diseño e implementación de una base de conocimiento y capacitación en SharePoint para nuevos integrantes del equipo de calidad, optimizando la curva de aprendizaje en un 60%.</p>
                 <span className="mt-1 block text-sm font-semibold text-blue-500">2020 - 2021</span>
               </div>
             </div>
